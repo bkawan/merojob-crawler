f23dd4063d17234af9415e10bca9358d45fedf17
Bikesh Kawan <bikeshkawang@gmail.com>
2017-09-15 13:18:15 +0545

    add get all job and job data

--- spider.py
+++ spider.py
@@ -1,7 +1,10 @@
 import re
+from datetime import datetime
 
 import requests
 from lxml import html
+
+search_link = 'https://merojob.com/search/'
 
 
 class MerojobSpider(object):
@@ -63,11 +66,178 @@ class MerojobSpider(object):
 
         return company_detail
 
+    def get_all_job_urls(self, search_link=search_link):
+        host = 'https://merojob.com'
+        jobs = []
+        response = requests.get(search_link)
+        etree = html.fromstring(response.content)
+        pagination_next = etree.xpath("//a[@class='pagination-next page-link']/@href")
+        if pagination_next:
+            pagination_next = '{}{}'.format(host, pagination_next[0])
+            jobs += self.get_all_job_urls(pagination_next)
+        job_links = etree.xpath("//h1[@itemprop='title']/a/@href")
+        jobs += job_links
+        return jobs
 
+    def get_salary(self, offered_salary):
+        minimum_salary = 0
+        maximum_salary = None
+        currency = None
+        salary_unit = None
 
+        if offered_salary:
+            salary = re.sub(r'[\s\n,]+', "", offered_salary)
+            if "Negotiable" not in salary:
+                salary = re.sub(r'[\s\n,]+', "", salary)
+                min_max = re.findall(r'[\d.]+', salary)
+                curr_range = re.findall(r'[a-zA-Z]+', salary)
+                minimum_salary = min_max[0].strip(".")
+                maximum_salary = None
+                if len(min_max) == 2:
+                    maximum_salary = min_max[1]
+                currency = curr_range[0]
+                salary_unit = curr_range[1]
+        return {
+            "minimum_salary": minimum_salary,
+            "maximum_salary": maximum_salary,
+            "currency": currency,
+            "salary_unit": salary_unit,
+        }
 
+    def get_job_data(self, job_url):
+        job_data = {}
+        response = requests.get(job_url)
+        etree = html.fromstring(response.content)
+        job_type = etree.xpath("//a[@class='ui right corner label']/i/@class")
+        job_type = job_type[0].split(" ")[0].replace('icon-', '')
+        title = etree.xpath("//h1[@itemprop='title']/text()")
+        employment_node = etree.xpath("//td[@itemprop='employmentType']")
+        employment_type = ['Full Time']
+        if employment_node:
+            employment_type = employment_node[0].xpath("string()")
+            employment_type = re.sub(r'[\n\s]+', '', employment_type).split(",")
+        date_posted_node = etree.xpath("//meta[@itemprop='datePosted']/@content")
+        if date_posted_node:
+            date_posted = date_posted_node[0].strip()
+        else:
+            date_posted = datetime.now()
+        deadline = etree.xpath("//meta[@itemprop='validThrough']/@content")
+        if deadline:
+            deadline = deadline[0].strip()
+        skills = etree.xpath("//span[@itemprop='skills']/span/text()")
+        job_description = etree.xpath("//div[@itemprop='description']")
+        if len(job_description) > 1:
+            job_description = job_description[1].xpath('string()')
+        else:
+            if job_type == 'newspaper':
+                job_description = job_description[0].xpath("string()")
+            else:
+                job_description = ""
+        job_data.update({
+            'title': title[0].strip(),
+            'date_posted': date_posted,
+            'deadine': deadline,
+            'skills': skills,
+            'job_description': job_description.strip(),
+            'job_type': job_type
+        })
 
+        table_etree = etree.xpath("//table")
+        for table in table_etree:
+            tr_etree = table.xpath(".//tr")
+            for tr in tr_etree:
+                td = tr.xpath(".//td")
+                key = td[0].xpath("string()").strip().lower().replace(" ", "_").strip(":")
+                value = td[1].xpath("string()").strip()
+                job_data.update({
+                    key: value
+                })
+        specification_node = etree.xpath("//div[@class='card-group']")
+        other_specification_node = None
+        for n in specification_node:
+            h6 = n.xpath(".//h6/text()")
+            if h6:
+                h6 = h6[0].strip()
+                if "Other Specification" in h6:
+                    other_specification_node = n
+        if other_specification_node:
+            other_specification = other_specification_node.xpath("string()").strip("Other Speficifation\n").strip()
+            job_data.update({
+                "other_specification": other_specification
+            })
+
+        job_category = job_data.get('job_category')
+        if job_category:
+            job_categories = job_category.split(">")
+            parent_job_category = job_categories[0].strip()
+            job_sub_category = None
+            if len(job_categories) > 1:
+                job_sub_category = job_categories[1].split(",")
+            job_data.update({
+                'employment_type': employment_type,
+                'job_category': {
+                    'parent': parent_job_category,
+                    'child': job_sub_category
+                },
+                'salary': self.get_salary(job_data.get('offered_salary'))
+            })
+
+        vacancies = 0
+        no_of_vacancy = job_data.get('no._of_vacancy/s')
+        if no_of_vacancy:
+            match = re.search(r'[\d]+', no_of_vacancy)
+            if match:
+                job_data.update({
+                    'vacancies': match.group()})
+
+        span_node = etree.xpath("//span/text()")
+        # print(span_node)
+        views = 0
+        views_list = []
+        for span in span_node:
+            if "Views: " in span:
+                span = span.strip()
+                count = re.findall(r'[\d]+', span)
+                if count:
+                    views_list.append(count[0])
+        views = views_list[0]
+        job_data.update({
+            'views': views
+        })
+
+        employer_name = etree.xpath("//span[@itemprop='name']/text()")
+        employer_url = None
+        if employer_name:
+            employer_name = employer_name[0]
+        else:
+            employer_name = etree.xpath("//h3[@class='h6']/a/text()")
+            if employer_name:
+                employer_name = employer_name[0].strip()
+        employer_url_node = etree.xpath("//h2[@class='ml-4 pl-5 pl-md-0 h5']/a/@href")
+        if employer_url_node:
+            employer_url = employer_url_node[0].strip()
+            employer_url = re.findall(r'employer/(.*)/?', employer_url)
+            if employer_url:
+                employer_url = employer_url[0].strip("/")
+
+        job_data.update({
+            'client': {
+                'employer_name': employer_name,
+                'employer_url': employer_url
+            }
+        })
+        slug_match = re.findall(r'com/(.*)/?', job_url)
+        if slug_match:
+            job_data.update({
+                'slug': slug_match[0].strip("/")
+            })
+
+        return job_data
+    
 if __name__ == "__main__":
     spider = MerojobSpider()
     all_company_url = spider.get_all_company_urls()
+    all_job_url = spider.get_all_job_urls()
+    for job in all_job_url[0:2]:
+        print(spider.get_job_data('{}{}'.format(spider.host, job)))
 
